ba59760a687716198bce0c77a22e3618e7df45ae
mokrzu <mokrzu@gmail.com>
2011-12-26 20:58:38 +0100

    initial commit.

---
+++ ScalaCode/src/code/Main.scala
@@ -0,0 +1,21 @@
+/**
+  *
+  */
+package code
+
+/**
+  * @author Mokrzu
+  *
+  */
+object Main {
+
+	def main(args: Array[String]): Unit = {
+		println("Hello!")
+		val x = readInt()
+		if (5 > x) {
+			println("OK")
+		} else {
+			println("No!!")
+		}
+	}
+}
